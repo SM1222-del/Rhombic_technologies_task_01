66fc71c2dbe0425d6214d9209231204baf5243bd
SM1222-del <sm1385442@gmail.com>
2024-12-20 21:22:27 -0800

    Update selenium_web.py

--- selenium_web.py
+++ selenium_web.py
@@ -1,5 +1,6 @@
 # selenium_web.py
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
 
 #class to construct a function
@@ -7,13 +8,13 @@ class Infos:
     def __init__(self):
 #initiate the driver
       self.query = None
-      self.driver = webdriver.Chrome() #full_path link of chromedriver in a string form
+      self.driver = webdriver.Chrome() #full_path link of chromedriver in a string format
 
     def get_Info(self,query):
         self.query=query
         self.driver.get(url="https://www.wikipedia.org")
-        search=self.driver.find_element_by_xpath('//*["@id=searchinput"]')
+        search=self.driver.find_element(By.XPATH,'//*["@id=searchinput"]')
         search.click()
         search.send_keys(query)
-        enter=self.driver.find_element_by_xpath('//*[@id="search-form"]/fieldsets/button')
+        enter=self.driver.find_element(By.XPATH,'//*[@id="search-form"]/fieldsets/button')
         enter.click()
